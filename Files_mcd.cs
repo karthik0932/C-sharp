cc33d4a8390627c3c44ad06af5ac8e3b41c199c7
karthik s <54766059+karthik0932@users.noreply.github.com>
2022-10-24 11:44:49 +0530

    Update Files_mcd.cs

--- Files_mcd.cs
+++ Files_mcd.cs
@@ -11,8 +11,8 @@ namespace TE_CSharp
     {
         public  static void Main()
         {
-            string source = @"C:\Users\ei13080\Desktop\Ujwala\Technical_Training\Others\Text1.txt";
-            string target = @"C:\Users\ei13080\Desktop\Ujwala\Technical_Training\Files\Text1.txt";
+            string source = @"C:\Users\ei13113\Desktop\karthik\Technical_Training\Others\Text1.txt";
+            string target = @"C:\Users\ei13113\Desktop\karthik\Technical_Training\Files\Text1.txt";
 
             //File.Move(source,target);
             //File.Copy(target, source);
